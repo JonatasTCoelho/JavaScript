a39ca1cabacded5989f252def55150d475e0b465
Jonatas Tonin Coelho <51675838+JonatasTCoelho@users.noreply.github.com>
2021-10-19 23:02:48 -0300

    Jogo Mata Mosquito

---
+++ Game_Mata_Mosquito/app2/jogo.js
@@ -0,0 +1,49 @@
+var altura = 0
+var largura = 0
+
+function ajustaTamanhoPalcoJogo(){
+    altura = window.innerHeight
+    largura = window.innerWidth
+
+    console.log(largura,altura)
+}
+
+ajustaTamanhoPalcoJogo()
+
+function posicaoRandomica(){
+
+    var positionX = Math.floor(Math.random() * largura) - 90
+    var positionY = Math.floor(Math.random() * altura) - 90
+    
+    positionX = positionX < 0 ? 0 : positionX
+    positionY = positionY < 0 ? 0 : positionY
+
+    console.log(positionX, positionY)
+    
+    //Criar o elemento HTML
+    var mosquito = document.createElement('img')
+    mosquito.src = '/imagens/imagens/mosca.png'
+    mosquito.className = tamanhoAleatorio()
+
+    mosquito.style.left = positionX + 'px'
+    mosquito.style.top = positionY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+    
+}
+
+function tamanhoAleatorio(){
+    var classe = Math.floor(Math.random() * 3)
+    
+    switch(classe){
+        case 0:
+            return 'mosquito1'
+        case 1:
+            return 'mosquito2'
+        case 2:
+            return 'mosquito3'
+    }
+}
+
+
